bc3222ec3dd7686e1779538c39f3e5d17a15b099
iPanda <aungkyawkhaing2000@gmail.com>
2022-10-25 21:31:27 +0630

    u

---
+++ src/@pages/navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const navbar = () => {
+  return (
+    <nav className='p-5'>
+        <Link>
+               dashboard    
+        </Link>
+        <Link>
+               Team
+        </Link>
+        <Link>
+               dashboard    
+        </Link>
+        <Link>
+               dashboard    
+        </Link>
+    </nav>
+  )
+}
+
+export default navbar
